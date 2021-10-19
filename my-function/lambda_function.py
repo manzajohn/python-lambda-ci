5720f99329848b4f75c6f9ce8cfb4cdabaae0485
manzajohn <30400777+manzajohn@users.noreply.github.com>
2021-10-19 22:47:04 +0530

    Updated lambda

--- my-function/lambda_function.py
+++ my-function/lambda_function.py
@@ -8,5 +8,7 @@ def lambda_handler(event, context):
     print("value1 = " + event['key1'])
     print("value2 = " + event['key2'])
     print("value3 = " + event['key3'])
+    print("context = " , context)
+    print(event)
     return event['key1']  # Echo back the first key value
     #raise Exception('Something went wrong')
